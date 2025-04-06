a02be12569ec919bef1b5530d2aa31850dbda822
YY-GX <yyang9923@gmail.com>
2025-04-06 14:19:35 -0400

    add shell for only wrist camera view

--- shells/train_oft_2_views.sh
+++ shells/train_oft_2_views.sh
@@ -1,60 +1,34 @@
-#!/bin/zsh
+#!/bin/bash
 
-echo "Submitting job..."
-
-
-# Define variables
 CPUS_PER_TASK=32
 GPUS=8
+JOB_NAME="finetune_openvla_2img"
 LOG_DIR="$ENDPOINT/pkgs_baselines/openvla-oft/logs"
-LOG_FILE="train_libero_local1_2_view_%j.out"
-JOB_NAME="openvla_libero_local1"
-SCRIPT_PATH="/mnt/arc/yygx/pkgs_baselines/openvla/vla-scripts/finetune.py"
-VLA_PATH="openvla/openvla-7b"
-DATA_ROOT_DIR="/mnt/arc/yygx/pkgs_baselines/openvla/datasets"
-DATASET_NAME="libero_local1"
-RUN_ROOT_DIR="/mnt/arc/yygx/pkgs_baselines/openvla/runs/view_2/1.0.0"
-LORA_RANK=32
-BATCH_SIZE=4
-LEARNING_RATE=5e-4
-IMAGE_AUG=True
-WANDB_PROJECT="openvla-oft-libero"
-WANDB_ENTITY="yygx"
-USE_L1_REGRESSION=True
-USE_DIFFUSION=False
-USE_FILM=False
-NUM_IMAGES_IN_INPUT=2
-USE_PROPRIO=True
-NUM_STEPS_BEFORE_DECAY=100000
-MAX_STEPS=150005
-SAVE_FREQ=10000
-SAVE_LATEST_CHECKPOINT_ONLY=False
-RUN_ID_NOTE="parallel_dec--8_acts_chunk--continuous_acts--L1_regression--3rd_person_img--wrist_img--proprio_state"
+LOG_FILE="train_libero_local1_2img_%j.out"
 
-# Submit the job
 sbatch \
   --cpus-per-task=$CPUS_PER_TASK \
   --gpus=$GPUS \
   -o "$LOG_DIR/$LOG_FILE" \
   -J $JOB_NAME \
-  --wrap="torchrun --standalone --nnodes 1 --nproc-per-node $GPUS $SCRIPT_PATH \
-    --vla_path $VLA_PATH \
-    --data_root_dir $DATA_ROOT_DIR \
-    --dataset_name $DATASET_NAME \
-    --run_root_dir $RUN_ROOT_DIR \
-    --use_l1_regression $USE_L1_REGRESSION \
-    --use_diffusion $USE_DIFFUSION \
-    --use_film $USE_FILM \
-    --num_images_in_input $NUM_IMAGES_IN_INPUT \
-    --use_proprio $USE_PROPRIO \
-    --batch_size $BATCH_SIZE \
-    --learning_rate $LEARNING_RATE \
-    --num_steps_before_decay $NUM_STEPS_BEFORE_DECAY \
-    --max_steps $MAX_STEPS \
-    --save_freq $SAVE_FREQ \
-    --save_latest_checkpoint_only $SAVE_LATEST_CHECKPOINT_ONLY \
-    --image_aug $IMAGE_AUG \
-    --lora_rank $LORA_RANK \
-    --wandb_entity $WANDB_ENTITY \
-    --wandb_project $WANDB_PROJECT \
-    --run_id_note \"$RUN_ID_NOTE\""
+  --wrap="torchrun --standalone --nnodes 1 --nproc-per-node 8 vla-scripts/finetune.py \
+    --vla_path openvla/openvla-7b \
+    --data_root_dir datasets \
+    --dataset_name libero_local1 \
+    --run_root_dir runs/view_2/1.0.0 \
+    --use_l1_regression True \
+    --use_diffusion False \
+    --use_film False \
+    --num_images_in_input 2 \
+    --use_proprio True \
+    --batch_size 4 \
+    --learning_rate 5e-4 \
+    --num_steps_before_decay 100000 \
+    --max_steps 150005 \
+    --save_freq 10000 \
+    --save_latest_checkpoint_only False \
+    --image_aug True \
+    --lora_rank 32 \
+    --wandb_entity 'yygx' \
+    --wandb_project 'openvla-oft-libero' \
+    --run_id_note 'parallel_dec--8_acts_chunk--continuous_acts--L1_regression--3rd_person_img--wrist_img--proprio_state'"
