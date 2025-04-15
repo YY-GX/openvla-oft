1918b31607e1549069cdb22dce4663c211264fa2
YY-GX <yyang9923@gmail.com>
2025-04-14 23:47:11 -0400

    comment oss shell lines

--- shells/evals/eval_oft_2_views.sh
+++ shells/evals/eval_oft_2_views.sh
@@ -12,18 +12,18 @@ CUDA_VISIBLE_DEVICES=5 python experiments/robot/libero/run_libero_eval_local.py 
   --wrist_only False &
 
 
-# With OSS
-CUDA_VISIBLE_DEVICES=6 python experiments/robot/libero/run_libero_eval_local.py \
-  --pretrained_checkpoint "/mnt/arc/yygx/pkgs_baselines/openvla-oft/runs/view_2/1.0.1/openvla-7b+libero_local2+b4+lr-0.0005+lora-r32+dropout-0.0--image_aug--parallel_dec--8_acts_chunk--continuous_acts--L1_regression--3rd_person_img--wrist_img--proprio_state--20000_chkpt" \
-  --task_suite_name "libero_local2" \
-  --wrist_only False \
-  --is_oss True &
-
-CUDA_VISIBLE_DEVICES=7 python experiments/robot/libero/run_libero_eval_local.py \
-  --pretrained_checkpoint "/mnt/arc/yygx/pkgs_baselines/openvla-oft/runs/view_2/1.0.1/openvla-7b+libero_local2+b4+lr-0.0005+lora-r32+dropout-0.0--image_aug--parallel_dec--8_acts_chunk--continuous_acts--L1_regression--3rd_person_img--wrist_img--proprio_state--50000_chkpt" \
-  --task_suite_name "libero_local2" \
-  --wrist_only False \
-  --is_oss True &
+## With OSS
+#CUDA_VISIBLE_DEVICES=6 python experiments/robot/libero/run_libero_eval_local.py \
+#  --pretrained_checkpoint "/mnt/arc/yygx/pkgs_baselines/openvla-oft/runs/view_2/1.0.1/openvla-7b+libero_local2+b4+lr-0.0005+lora-r32+dropout-0.0--image_aug--parallel_dec--8_acts_chunk--continuous_acts--L1_regression--3rd_person_img--wrist_img--proprio_state--20000_chkpt" \
+#  --task_suite_name "libero_local2" \
+#  --wrist_only False \
+#  --is_oss True &
+#
+#CUDA_VISIBLE_DEVICES=7 python experiments/robot/libero/run_libero_eval_local.py \
+#  --pretrained_checkpoint "/mnt/arc/yygx/pkgs_baselines/openvla-oft/runs/view_2/1.0.1/openvla-7b+libero_local2+b4+lr-0.0005+lora-r32+dropout-0.0--image_aug--parallel_dec--8_acts_chunk--continuous_acts--L1_regression--3rd_person_img--wrist_img--proprio_state--50000_chkpt" \
+#  --task_suite_name "libero_local2" \
+#  --wrist_only False \
+#  --is_oss True &
 
 # Wait for both processes to finish
 wait
--- shells/evals/eval_oft_wrist_view.sh
+++ shells/evals/eval_oft_wrist_view.sh
@@ -12,18 +12,18 @@ CUDA_VISIBLE_DEVICES=1 python experiments/robot/libero/run_libero_eval_local.py 
   --wrist_only True &
 
 
-# With OSS
-CUDA_VISIBLE_DEVICES=2 python experiments/robot/libero/run_libero_eval_local.py \
-  --pretrained_checkpoint "/mnt/arc/yygx/pkgs_baselines/openvla-oft/runs/view_wrist/1.0.1/openvla-7b+libero_local2+b4+lr-0.0005+lora-r32+dropout-0.0--image_aug--parallel_dec--8_acts_chunk--continuous_acts--L1_regression--wrist_img--proprio_state--20000_chkpt" \
-  --task_suite_name "libero_local2" \
-  --wrist_only True \
-  --is_oss True &
-
-CUDA_VISIBLE_DEVICES=3 python experiments/robot/libero/run_libero_eval_local.py \
-  --pretrained_checkpoint "/mnt/arc/yygx/pkgs_baselines/openvla-oft/runs/view_wrist/1.0.1/openvla-7b+libero_local2+b4+lr-0.0005+lora-r32+dropout-0.0--image_aug--parallel_dec--8_acts_chunk--continuous_acts--L1_regression--wrist_img--proprio_state--50000_chkpt" \
-  --task_suite_name "libero_local2" \
-  --wrist_only True \
-  --is_oss True &
+## With OSS
+#CUDA_VISIBLE_DEVICES=2 python experiments/robot/libero/run_libero_eval_local.py \
+#  --pretrained_checkpoint "/mnt/arc/yygx/pkgs_baselines/openvla-oft/runs/view_wrist/1.0.1/openvla-7b+libero_local2+b4+lr-0.0005+lora-r32+dropout-0.0--image_aug--parallel_dec--8_acts_chunk--continuous_acts--L1_regression--wrist_img--proprio_state--20000_chkpt" \
+#  --task_suite_name "libero_local2" \
+#  --wrist_only True \
+#  --is_oss True &
+#
+#CUDA_VISIBLE_DEVICES=3 python experiments/robot/libero/run_libero_eval_local.py \
+#  --pretrained_checkpoint "/mnt/arc/yygx/pkgs_baselines/openvla-oft/runs/view_wrist/1.0.1/openvla-7b+libero_local2+b4+lr-0.0005+lora-r32+dropout-0.0--image_aug--parallel_dec--8_acts_chunk--continuous_acts--L1_regression--wrist_img--proprio_state--50000_chkpt" \
+#  --task_suite_name "libero_local2" \
+#  --wrist_only True \
+#  --is_oss True &
 
 # Wait for both processes to finish
 wait
