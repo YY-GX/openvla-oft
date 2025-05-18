522edb96b02b61ec4cd674bc10fe3971fde94aa9
yy-gx <yyang9923@gmail.com>
2025-05-18 18:58:05 -0400

    update training zsh shells - 1.0.3 for 44 skills training

--- prismatic/vla/datasets/rlds/oxe/transforms.py
+++ prismatic/vla/datasets/rlds/oxe/transforms.py
@@ -928,6 +928,7 @@ OXE_STANDARDIZATION_TRANSFORMS = {
     "libero_local1": libero_dataset_transform,
     "libero_local2": libero_dataset_transform,
     "libero_local3": libero_dataset_transform,
+    "libero44_local": libero_dataset_transform,
     ### ALOHA fine-tuning datasets
     "aloha1_fold_shorts_20_demos": aloha_dataset_transform,
     "aloha1_fold_shirt_30_demos": aloha_dataset_transform,
